2ecf9e31e56a2da042d607be4c65ac288acf5721
Daniel Kluver <kluve018@umn.edu>
2025-11-11 09:14:00 -0600

    setup for hosting

--- src/views/FunView.vue
+++ src/views/FunView.vue
@@ -1,7 +1,17 @@
 <script setup>
 import { ref } from 'vue'
+import { useCollection } from 'vuefire'
+import { collection } from 'firebase/firestore'
+import {db} from '../firebase_conf'
+
+const tests = useCollection(collection(db, 'tests'))
+
 </script>
 
-<template>THE PAGE</template>
+<template>
+    <div v-for="test in tests" :key="test.id">
+        <span>{{test.name}}</span>
+    </div>
+</template>
 
 <style scoped></style>
